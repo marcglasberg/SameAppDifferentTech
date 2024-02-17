7c8b9d5fa1b48cd8ede08ba1a993efcf310df6d3
Marcelo Glasberg <13332110+marcglasberg@users.noreply.github.com>
2024-02-17 12:44:55 -0300

    Basic structure.

--- MobileAppReactNativeRedux/index.js
+++ MobileAppReactNativeRedux/index.js
@@ -1,9 +1,21 @@
-/**
- * @format
- */
+import { AppRegistry, Platform, UIManager } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
+// import { developmentConfiguration, inject } from './src/inject';
+// import { SimulatedDao } from './src/business/dao/SimulatedDao';
+// import { Storage } from './src/business/dao/Storage';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// inject({
+//   dao: new SimulatedDao(),
+//   storage: new Storage(),
+//   runConfig: developmentConfiguration,
+//   // runConfig: anotherConfiguration,
+// });
+
+// Enables layout animation feature which allows for smooth automatic animations during layout changes,
+// such as when new views are added or existing ones are removed. This is necessary because, by default,
+// layout animations are not enabled on Android as they are on iOS.
+if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental)
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ MobileAppReactNativeRedux/src/counterSlice.ts
@@ -0,0 +1,31 @@
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+export interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = { value: 0 };
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      // Uses the Immer library.
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    }
+  }
+});
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount }
+  = counterSlice.actions;
+
+export default counterSlice.reducer;
